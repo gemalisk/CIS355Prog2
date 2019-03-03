a9e04dcada53a3f8cf8d8f0d46d6c3b67b656766
gemalisk <43007423+gemalisk@users.noreply.github.com>
2019-03-03 13:22:05 -0500

    Redid Project Structure to Match Corser

--- customer_oo/customer.class.php
+++ customer_oo/customer.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class Customer { 
+class Customer {
     public $id;
     public $name;
     public $email;
@@ -14,70 +14,51 @@ class Customer {
     
     function create_record() { // display "create" form
         $this->generate_html_top (1);
-        $this->generate_form_group("name", $this->nameError, $this->name, "autofocus");
-        $this->generate_form_group("email", $this->emailError, $this->email);
-        $this->generate_form_group("mobile", $this->mobileError, $this->mobile);
+        $this->control_group("name", $this->nameError, $this->name);
+        $this->control_group("email", $this->emailError, $this->email);
+        $this->control_group("mobile", $this->mobileError, $this->mobile);
         $this->generate_html_bottom (1);
     } // end function create_record()
     
     function read_record($id) { // display "read" form
         $this->select_db_record($id);
         $this->generate_html_top(2);
-        $this->generate_form_group("name", $this->nameError, $this->name, "disabled");
-        $this->generate_form_group("email", $this->emailError, $this->email, "disabled");
-        $this->generate_form_group("mobile", $this->mobileError, $this->mobile, "disabled");
+        $this->control_group("name", $this->nameError, $this->name, "readonly");
+        $this->control_group("email", $this->emailError, $this->email, "readonly");
+        $this->control_group("mobile", $this->mobileError, $this->mobile, "readonly");
         $this->generate_html_bottom(2);
     } // end function read_record()
     
     function update_record($id) { // display "update" form
         if($this->noerrors) $this->select_db_record($id);
         $this->generate_html_top(3, $id);
-        $this->generate_form_group("name", $this->nameError, $this->name, "autofocus onfocus='this.select()'");
-        $this->generate_form_group("email", $this->emailError, $this->email);
-        $this->generate_form_group("mobile", $this->mobileError, $this->mobile);
+        $this->control_group("name", $this->nameError, $this->name);
+        $this->control_group("email", $this->emailError, $this->email);
+        $this->control_group("mobile", $this->mobileError, $this->mobile);
         $this->generate_html_bottom(3);
     } // end function update_record()
     
     function delete_record($id) { // display "read" form
         $this->select_db_record($id);
         $this->generate_html_top(4, $id);
-        $this->generate_form_group("name", $this->nameError, $this->name, "disabled");
-        $this->generate_form_group("email", $this->emailError, $this->email, "disabled");
-        $this->generate_form_group("mobile", $this->mobileError, $this->mobile, "disabled");
+        $this->control_group("name", $this->nameError, $this->name, "readonly");
+        $this->control_group("email", $this->emailError, $this->email, "readonly");
+        $this->control_group("mobile", $this->mobileError, $this->mobile, "readonly");
         $this->generate_html_bottom(4);
     } // end function delete_record()
     
-    /*
-     * This method inserts one record into the table, 
-     * and redirects user to List, IF user input is valid, 
-     * OTHERWISE it redirects user back to Create form, with errors
-     * - Input: user data from Create form
-     * - Processing: INSERT (SQL)
-     * - Output: None (This method does not generate HTML code,
-     *   it only changes the content of the database)
-     * - Precondition: Public variables set (name, email, mobile)
-     *   and database connection variables are set in datase.php.
-     *   Note that $id will NOT be set because the record 
-     *   will be a new record so the SQL database will "auto-number"
-     * - Postcondition: New record is added to the database table, 
-     *   and user is redirected to the List screen (if no errors), 
-     *   or Create form (if errors)
-     */
     function insert_db_record () {
-        if ($this->fieldsAllValid ()) { // validate user input
-            // if valid data, insert record into table
+        if ($this->fieldsAllValid ()) {
             $pdo = Database::connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "INSERT INTO $this->tableName (name,email,mobile) values(?, ?, ?)";
             $q = $pdo->prepare($sql);
             $q->execute(array($this->name,$this->email,$this->mobile));
             Database::disconnect();
-            header("Location: $this->tableName.php"); // go back to "list"
+            header("Location: $this->tableName.php");
         }
         else {
-            // if not valid data, go back to "create" form, with errors
-            // Note: error fields are set in fieldsAllValid ()method
-            $this->create_record(); 
+            $this->create_record(); // go back to "create" form
         }
     } // end function insert_db_record
     
@@ -125,20 +106,19 @@ class Customer {
     private function generate_html_top ($fun, $id=null) {
         switch ($fun) {
             case 1: // create
-                $funWord = "Create"; $funNext = "insert_db_record"; 
+                $funWord = "Create"; $funNext = 11; 
                 break;
             case 2: // read
-                $funWord = "Read"; $funNext = "none"; 
+                $funWord = "Read"; $funNext = 0; 
                 break;
             case 3: // update
-                $funWord = "Update"; $funNext = "update_db_record&id=" . $id; 
+                $funWord = "Update"; $funNext = "33&id=" . $id; 
                 break;
             case 4: // delete
-                $funWord = "Delete"; $funNext = "delete_db_record&id=" . $id; 
+                $funWord = "Delete"; $funNext = "44&id=" . $id; 
                 break;
-            default: 
-                echo "Error: Invalid function: generate_html_top()"; 
-                exit();
+            case 0: // list
+            default:
                 break;
         }
         echo "<!DOCTYPE html>
@@ -150,11 +130,10 @@ class Customer {
                 <meta charset='UTF-8'>
                 <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
                 <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
-                <style>label {width: 5em;}</style>
                     "; 
         echo "
             </head>";
-        echo "
+                echo "
             <body>
                 <div class='container'>
                     <div class='span10 offset1'>
@@ -179,9 +158,8 @@ class Customer {
             case 4: // delete
                 $funButton = "<button type='submit' class='btn btn-danger'>Delete</button>"; 
                 break;
-            default: 
-                echo "Error: Invalid function: generate_html_bottom()"; 
-                exit();
+            case 0: // list
+            default: // list
                 break;
         }
         echo " 
@@ -198,12 +176,12 @@ class Customer {
                     ";
     } // end function generate_html_bottom()
     
-    private function generate_form_group ($label, $labelError, $val, $modifier="") {
-        echo "<div class='form-group'";
+    private function control_group ($label, $labelError, $val, $modifier="") {
+        echo "<div class='control-group";
         echo !empty($labelError) ? ' alert alert-danger ' : '';
         echo "'>";
-        echo "<label class='control-label'>$label &nbsp;</label>";
-        //echo "<div class='controls'>";
+        echo "<label class='control-label'>$label</label>";
+        echo "<div class='controls'>";
         echo "<input "
             . "name='$label' "
             . "type='text' "
@@ -217,9 +195,9 @@ class Customer {
             echo "&nbsp;&nbsp;" . $labelError;
             echo "</span>";
         }
-        //echo "</div>"; // end div: class='controls'
-        echo "</div>"; // end div: class='form-group'
-    } // end function generate_form_group()
+        echo "</div>";
+        echo "</div>";
+    } // end function control_group()
     
     private function fieldsAllValid () {
         $valid = true;
@@ -236,7 +214,7 @@ class Customer {
             $valid = false;
         }
         if (empty($this->mobile)) {
-            $this->mobileError = 'Please enter Mobile phone number';
+            $this->mobileError = 'Please enter Mobile Number';
             $valid = false;
         }
         return $valid;
@@ -256,13 +234,13 @@ class Customer {
         echo "
             </head>
             <body>
-                <a href='https://github.com/cis355/PhpProject1' target='_blank'>Github</a><br />
+				<a href='https://github.com/gemalisk/CIS355Prog2' target='_blank'>Github</a><br />
                 <div class='container'>
                     <p class='row'>
                         <h3>$this->title" . "s" . "</h3>
                     </p>
                     <p>
-                        <a href='$this->tableName.php?fun=display_create_form' class='btn btn-success'>Create</a>
+                        <a href='$this->tableName.php?fun=1' class='btn btn-success'>Create</a>
                     </p>
                     <div class='row'>
                         <table class='table table-striped table-bordered'>
@@ -284,11 +262,11 @@ class Customer {
             echo "<td>". $row["email"] . "</td>";
             echo "<td>". $row["mobile"] . "</td>";
             echo "<td width=250>";
-            echo "<a class='btn btn-info' href='$this->tableName.php?fun=display_read_form&id=".$row["id"]."'>Read</a>";
+            echo "<a class='btn btn-info' href='$this->tableName.php?fun=2&id=".$row["id"]."'>Read</a>";
             echo "&nbsp;";
-            echo "<a class='btn btn-warning' href='$this->tableName.php?fun=display_update_form&id=".$row["id"]."'>Update</a>";
+            echo "<a class='btn btn-warning' href='$this->tableName.php?fun=3&id=".$row["id"]."'>Update</a>";
             echo "&nbsp;";
-            echo "<a class='btn btn-danger' href='$this->tableName.php?fun=display_delete_form&id=".$row["id"]."'>Delete</a>";
+            echo "<a class='btn btn-danger' href='$this->tableName.php?fun=4&id=".$row["id"]."'>Delete</a>";
             echo "</td>";
             echo "</tr>";
         }
